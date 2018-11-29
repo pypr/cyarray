b89b04dace285d9e4c3651c0e953feac0f05f9eb
Prabhu Ramachandran <prabhu@aero.iitb.ac.in>
2018-11-29 16:13:04 -0500

    Add basic files for a separate cyarray package.

---
+++ README.rst
@@ -0,0 +1,53 @@
+CyArray: a typed, re-sizable Cython array
+------------------------------------------
+
+The CyArray package provides a fast, typed, re-sizable, Cython array.
+
+It currently provides the following arrays: ``IntArray, UIntArray, LongArray,
+FloatArray, DubleArray``.
+
+All arrays provide for the following operations:
+
+ - access by indexing.
+ - access through get/set function.
+ - resizing the array.
+ - appending values at the end of the array.
+ - reserving space for future appends.
+ - access to internal data through a numpy array.
+
+If you are writing Cython code this is a convenient array to use as it exposes
+the raw underlying pointer to the data. For example if you use a ``FloatArray``
+and access its ``data`` attribute it will be a ``float*``.
+
+Each array also provides an interface to its data through a numpy array.
+This is done through the ``get_npy_array`` function. The returned numpy
+array can be used just like any other numpy array but for the following
+restrictions:
+
+ - the array may not be resized.
+ - references of this array should not be kept.
+ - slices of this array may not be made.
+
+The numpy array may however be copied and used in any manner.
+
+Installation
+------------
+
+CyArray can be installed using pip_::
+
+  $ pip install cyarray
+
+The package requires ``Cython``, ``numpy``, and ``mako`` to be installed and
+also requires a suitably configured C/C++ compiler.
+
+.. _pip: http://www.pip-installer.org
+
+Usage
+-----
+
+In Python one may import and use the package as::
+
+  from cyarray.api import IntArray
+  a = IntArray(10)
+
+Here ``a`` is an array of 10 integers.
---
+++ setup.py
@@ -0,0 +1,157 @@
+import os
+import sys
+from subprocess import check_output
+
+
+if len(os.environ.get('COVERAGE', '')) > 0:
+    MACROS = [("CYTHON_TRACE", "1"), ("CYTHON_TRACE_NOGIL", "1")]
+    COMPILER_DIRECTIVES = {"linetrace": True}
+    print("-" * 80)
+    print("Enabling linetracing for cython and setting CYTHON_TRACE = 1")
+    print("-" * 80)
+else:
+    MACROS = []
+    COMPILER_DIRECTIVES = {}
+
+MODE = 'normal'
+if len(sys.argv) >= 2 and \
+   ('--help' in sys.argv[1:] or
+    sys.argv[1] in ('--help-commands', 'egg_info', '--version',
+                    'clean', 'sdist')):
+    MODE = 'info'
+
+
+def get_basic_extensions():
+    if MODE == 'info':
+        try:
+            from Cython.Distutils import Extension
+        except ImportError:
+            from distutils.core import Extension
+        try:
+            import numpy
+        except ImportError:
+            include_dirs = []
+        else:
+            include_dirs = [numpy.get_include()]
+    else:
+        from Cython.Distutils import Extension
+        import numpy
+        include_dirs = [numpy.get_include()]
+
+    ext_modules = [
+        Extension(
+            name="cyarray.carray",
+            sources=["cyarray/carray.pyx"],
+            include_dirs=include_dirs,
+            language="c++"
+        ),
+    ]
+    return ext_modules
+
+
+def create_sources():
+    argv = sys.argv
+    if 'build_ext' in argv or 'develop' in sys.argv or 'install' in argv:
+        generator = os.path.join('cyarray', 'generator.py')
+        cmd = [sys.executable, generator, os.path.abspath('cyarray')]
+        print(check_output(cmd).decode())
+
+
+def _is_cythonize_default():
+    import warnings
+    result = True
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        try:
+            # old_build_ext was introduced in Cython 0.25 and this is when
+            # cythonize was made the default.
+            from Cython.Distutils import old_build_ext  # noqa: F401
+        except ImportError:
+            result = False
+    return result
+
+
+def setup_package():
+    from setuptools import find_packages, setup
+    if MODE == 'info':
+        cmdclass = {}
+    else:
+        from Cython.Distutils import build_ext
+        cmdclass = {'build_ext': build_ext}
+
+    create_sources()
+
+    # Extract the version information from pysph/__init__.py
+    info = {}
+    module = os.path.join('cyarray', '__init__.py')
+    exec(compile(open(module).read(), module, 'exec'), info)
+
+    # The requirements.
+    install_requires = [
+        'numpy', 'mako', 'Cython>=0.20', 'setuptools>=6.0',
+        'pytest>=3.0'
+    ]
+    ext_modules = get_basic_extensions()
+    if MODE != 'info' and _is_cythonize_default():
+        # Cython >= 0.25 uses cythonize to compile the extensions. This
+        # requires the compile_time_env to be set explicitly to work.
+        compile_env = {}
+        include_path = set()
+        for mod in ext_modules:
+            compile_env.update(mod.cython_compile_time_env or {})
+            include_path.update(mod.include_dirs)
+        from Cython.Build import cythonize
+        ext_modules = cythonize(
+            ext_modules, compile_time_env=compile_env,
+            include_path=list(include_path),
+            language="c++",
+            compiler_directives=COMPILER_DIRECTIVES,
+        )
+
+    setup(name='CyArray',
+          version=info['__version__'],
+          author='Cyarray Developers',
+          author_email='pysph-dev@googlegroups.com',
+          description='A fast, typed, resizable, Cython array.',
+          long_description=open('README.rst').read(),
+          url='http://github.com/pypr/cyarray',
+          license="BSD",
+          keywords="Cython array resizable",
+          setup_requires=['pytest-runner'],
+          tests_require=['pytest'],
+          packages=find_packages(),
+          package_data={
+              '': ['*.pxd', '*.mako', '*.rst']
+          },
+          # exclude package data in installation.
+          exclude_package_data={
+              '': ['Makefile', '*.bat', '*.cfg', '*.rst', '*.sh', '*.yml'],
+          },
+          ext_modules=ext_modules,
+          include_package_data=True,
+          cmdclass=cmdclass,
+          install_requires=install_requires,
+          zip_safe=False,
+          platforms=['Linux', 'Mac OS-X', 'Unix', 'Windows'],
+          classifiers=[c.strip() for c in """\
+            Development Status :: 5 - Production/Stable
+            Environment :: Console
+            Intended Audience :: Developers
+            Intended Audience :: Science/Research
+            License :: OSI Approved :: BSD License
+            Natural Language :: English
+            Operating System :: MacOS :: MacOS X
+            Operating System :: Microsoft :: Windows
+            Operating System :: POSIX
+            Operating System :: Unix
+            Programming Language :: Python
+            Programming Language :: Python :: 3
+            Topic :: Scientific/Engineering
+            Topic :: Scientific/Engineering :: Physics
+            Topic :: Software Development :: Libraries
+            """.splitlines() if len(c.split()) > 0],
+          )
+
+
+if __name__ == '__main__':
+    setup_package()
